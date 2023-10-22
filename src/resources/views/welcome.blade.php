9d56ba86c1c4e97abec9d531537dd1ff2cb3f8b0
Your Name <nt@nt>
2023-10-22 21:14:39 +0300

    add color names

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -4,10 +4,10 @@
 </head>
 <body>
 <div id="app">
-    <div>UserName: {{Auth::user()->name}}</div>
+    My name: <div id="name">{{Auth::user()->name}}</div>
     <div id="chat" style="width: 100%; height: auto; min-height: 500px; border: solid 1px black; margin-bottom: 5px;"></div>
-        <input id="text" type="text" style="width: 100%; margin-bottom: 5px;">
-        <input id="submit" type="button" value="send">
+        <input id="text" type="text" autocomplete="off" aria-autocomplete="none" style="width: 100%; margin-bottom: 5px;">
+        <input id="submit" type="button" value="send" >
 </div>
 @vite('resources/js/app.js')
 </body>
